a5ed62ae6543408d4d73a34c0111c8cc9a88efdb
Junho <hjhxiaowu@163.com>
2015-09-20 22:32:19 +0800

    修改hbkTest2.py

--- hbkTest2.py
+++ hbkTest2.py
@@ -3,25 +3,23 @@
 __author__ = 'Junho'
 
 import urllib,urllib2
-import re
+import threading,time
 from lxml import etree
 
-url = 'http://www.heibanke.com/lesson/crawler_ex01/'
+def test():
+    url = 'http://www.heibanke.com/lesson/crawler_ex01/'
+    for i in range(30):
+        test_data = {'username':'123', 'password': '%d' % i}
+        post_data = urllib.urlencode(test_data)
+        f = urllib2.urlopen(url, post_data)
+        html = f.read()
+        page = etree.HTML(html)
+        h3 = page.xpath(u'//h3')
+        for h in h3:
+            text = h.text
+        f.close()
+        if text != u'您输入的密码错误, 请重新输入':
+            print '测试成功'
+            break
 
-for i in range(31):
-    test_data = {'username':'123', 'password': '%d' % i}
-    post_data = urllib.urlencode(test_data)
-    f = urllib2.urlopen(url, post_data)
-    html = f.read()
-    page = etree.HTML(html)
-    h3 = page.xpath(u'//h3')
-    for h in h3:
-        text = h.text
-    f.close()
-    if text != u'您输入的密码错误, 请重新输入':
-        print text,i
-        print html
-        print '测试成功'
-        break
-    print text,i
-
+test()
