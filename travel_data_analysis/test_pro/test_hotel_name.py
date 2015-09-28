d0fa1a48f47dd29359c7f73c6f084bc5cd3f38df
Junho <hjhxiaowu@163.com>
2015-09-28 23:24:29 +0800

    蚂蜂窝爬虫学习

--- travel_data_analysis/test_pro/test_hotel_name.py
+++ travel_data_analysis/test_pro/test_hotel_name.py
@@ -22,16 +22,16 @@ class Hotel():
         self.priceInfo = priceInfo
 
     def printHotel(self):
-        print u'酒店名称：' + self.name
-        print u'评论条数：' + str(self.comments) + u'条'
-        print u'游记提及条数：' + str(self.travelNotes) + u'条'
-        print u'位于区域：' + self.area
-        print u'最低价格：' + str(self.priceInfo) + u'元'
+        print u'酒店名称：%s' % self.name
+        print u'评论条数：%s' % self.comments
+        print u'游记提及条数：%s' % self.travelNotes
+        print u'位于区域：%s' % self.area
+        print u'最低价格：%s元' % self.priceInfo
 
 # -------------------------解析方法-------------------------- #
 # 获取page
 def getPage(url):
-    html = requests.get(url)
+    html = requests.get(url, timeout=30)
     return etree.HTML(html.text)
 
 # 爬取酒店名称
@@ -41,23 +41,38 @@ def getNames(page):
 
 # 爬取评论条数
 def getComments(page):
-    comments = page.xpath('//*[@id="hotel_list"]/div[@class="hotel-item clearfix h-item"]/div[3]/ul/li[1]/a/em/text()')
-    for index,item in enumerate(comments):
-        tmp = item.replace('条', '')
-        print tmp
-        comments[index] = int(tmp)
+    comments = []
+    content_field = page.xpath('//div[@class="hotel-list"]/div[@class="hotel-item clearfix h-item"]')
+    for each in content_field:
+        comment = each.xpath('div[@class="hotel-info"]/ul/li[1]/a/em/text()')
+        if comment:
+            comments.append(comment[0])
+        else:
+            comments.append('0条')
     return comments
 
 # 爬取游记提及条数
 def getTravelNotes(page):
-    travelNotes = page.xpath('//*[@id="hotel_list"]/div[@class="hotel-item clearfix h-item"]/div[3]/ul/li[3]/a/em/text()')
-    for index,item in enumerate(travelNotes):
-        travelNotes[index] = int(item.replace('条', ''))
+    travelNotes = []
+    content_field = page.xpath('//div[@class="hotel-list"]/div[@class="hotel-item clearfix h-item"]')
+    for each in content_field:
+        travelNote = each.xpath('div[@class="hotel-info"]/ul/li[3]/a/em/text()')
+        if travelNote:
+            travelNotes.append(travelNote[0])
+        else:
+            travelNotes.append('0篇')
     return travelNotes
 
 # 爬取酒店位于区域
 def getAreas(page):
-    areas = page.xpath('//*[@id="hotel_list"]/div[@class="hotel-item clearfix h-item"]/div[3]/div/span/a/text()')
+    areas = []
+    content_field = page.xpath('//div[@class="hotel-list"]/div[@class="hotel-item clearfix h-item"]/div[3]/div/span')
+    for each in content_field:
+        area = each.xpath('a/text()')
+        if area:
+            areas.append(area[0])
+        else:
+            areas.append('none')
     return areas
 
 # 爬取最低价格信息
@@ -79,6 +94,8 @@ def getHotels(url):
     travelNotes = getTravelNotes(page)
     priceInfos = getPriceInfos(page)
     areas = getAreas(page)
+    # length = len(names)
+    # print length
     for i in range(20):
         hotel = Hotel(names[i], comments[i], travelNotes[i], areas[i], priceInfos[i])
         hotels.append(hotel)
@@ -89,7 +106,8 @@ if __name__ == '__main__':
     start2 = time.time()
     pool = Pool(4)
     urllist = []
-    for i in range(7):
+    for i in range(1, 11):
+        print i
         url = 'http://www.mafengwo.cn/hotel/11053/?sFrom=mdd#indate=2015-09-28&outdate=2015-09-29&q=&p=%s&scope=city%%2C0%%2C&sort=comment_desc&sales=0&price=0%%2C' % i
         urllist.append(url)
     # 多进程测试
