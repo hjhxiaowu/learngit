863b5424a6e7f1b8b19abcd704f6028d4d6d4374
hjhxiaowu <hjhxiaowu@163.com>
2015-10-17 23:30:08 +0800

    2015/10/15宿舍：完成城市信息爬取（动态网页）

--- travel_data_analysis/test_pro/test_country.py
+++ travel_data_analysis/test_pro/test_country.py
@@ -8,13 +8,18 @@ purpose    : 蚂蜂窝酒店数据爬虫项目——国外城市测试
 date       : 2015/10/15
 author     : junho
 --------------------------------------------------------
+2015/10/17 : 爬取城市信息（动态网页获取json）
 ########################################################
 """
 
+import json
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from multiprocessing import Pool
 from lxml import etree
 import requests
 import time
+import MySQLdb
 import pandas as pd
 import sys
 reload(sys)
@@ -28,9 +33,6 @@ class Country():
         self.name = name
         self.city = city
         self.personNum = personNum
-
-    def __init__(self, name):
-        self.name = name
 
     def printCountry(self):
         print u'国家名称：%s' % self.name
@@ -50,53 +52,108 @@ def getPage(url):
 
 # 获取国家信息：所属洲，国家名称，国家链接
 def getCountryInfo(page):
-    content_field = page.xpath('//div[@class="container"]/div[7]/div/div[1]/div/dl')
+    content_field = page.xpath('//div[@class="container"]/div[6]/div/div[1]/div/dl')
     names = []
     deltas = []
-    hrefs = []
+    countryids = []
     for each in content_field:
         delta = each.xpath('dt/text()')[0]
         tmp_names = each.xpath('dd/ul/li/a/text()')
-        tmp_hrefs = each.xpath('dd/ul/li/a/@href')
+        tmp_countryids = each.xpath('dd/ul/li/a/@href')
         for i in range(len(tmp_names)):
             names.append(tmp_names[i].strip())
             deltas.append(delta)
-            hrefs.append(str(tmp_hrefs[i])[-10:-5])
-    return deltas, names, hrefs
+            countryids.append(str(tmp_countryids[i])[-10:-5])
+    return deltas, names, countryids
 
 # 获取城市信息
-def getCityInfo(page):
-    # //*[@id="poi_146952"]/h3/a
-    # //*[@id="poiList"]
-    citys = page.xpath('//*[@id="poiList"]/li/h3/a/text()')
-    personNums = page.xpath('//*[@id="poiList"]/li/p[1]/span/text()')
-    print len(citys), len(personNums)
+def getCityInfo(countryids):
+    citys = []
+    personNums = []
+    url = 'http://www.mafengwo.cn/gonglve/sg_ajax.php?sAct=getMapData&iMddid=%s&iType=3&iPage=1' % countryids
+    html = requests.get(url, timeout=30)
+    page = html.content.encode('utf-8')
+    target = json.loads(page)
+    for i in range(len(target['list'])):
+        citys.append(target['list'][i]['name'])
+        personNums.append(target['list'][i]['rank'])
     return citys, personNums
 
+def getCityInfo2(countryids):
+    citys = []
+    personNums = []
+    url = 'http://www.mafengwo.cn/gonglve/sg_ajax.php?sAct=getMapData&iMddid=%s&iType=3&iPage=1' % countryids
+    html = requests.get(url, timeout=30)
+    page = html.content.encode('utf-8')
+    target = json.loads(page)
+    for i in range(len(target['list'])):
+        citys.append(target['list'][i]['name'])
+        personNums.append(target['list'][i]['rank'])
+    return citys, personNums
 
 # 国外城市爬取信息汇总
 def getCountrys(url):
     page = getPage(url)
     countrys = []
-    tmp_deltas, tmp_names, hrefs = getCountryInfo(page)
-    for i in range(len(hrefs)):
-        page_in = getPage(strUrl + hrefs[i])
-        print strUrl + hrefs[i]
-        tmp_citys, tmp_personNums = getCityInfo(page_in)
+    tmp_deltas, tmp_names, tmp_countryids = getCountryInfo(page)
+    for i in range(len(tmp_countryids)):
+        tmp_citys, tmp_personNums = getCityInfo(tmp_countryids[i])
         for j in range(len(tmp_citys)):
-            countrys = Country(tmp_deltas[i], tmp_names[i], tmp_citys[j], tmp_personNums[j])
+            country = Country(tmp_deltas[i], tmp_names[i], tmp_citys[j], tmp_personNums[j])
+            countrys.append(country)
     return countrys
 
-# 测试案例1:
+# 测试案例1:导入pandas
 def test1():
+    start = time.time()
     url = 'http://www.mafengwo.cn/mdd/'
     countrys = getCountrys(url)
     countrylists = []
     for c in countrys:
         countrylists.append(c.getCountry())
     df = pd.DataFrame(countrylists, columns=['delta', 'name', 'city', 'personNum'])
+    print 'len(df):%s' % len(df)
+    end = time.time()
+    print u'导入pandas执行时间：%s' % (end - start)
     print df
+
+
+# 测试城市信息爬取
+def test2():
+    countryids = [10184, 10183]
+    tmp_citys, tmp_personNums = getCityInfo2(countryids[0])
+    for i in range(len(tmp_personNums)):
+        print u'韩国', tmp_citys[i], tmp_personNums[i]
+    tmp_citys, tmp_personNums = getCityInfo2(countryids[1])
+    for i in range(len(tmp_personNums)):
+        print u'日本', tmp_citys[i], tmp_personNums[i]
+
+# 测试案例3：导入mysql数据库
+def test3():
+    try:
+        conn = MySQLdb.connect(host="localhost", user="root", passwd="544989", db="python_pro", charset="utf8")
+        print u'mysql测试正常'
+        conn.close()
+    except MySQLdb.Error, e:
+        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+    start = time.time()
+    url = 'http://www.mafengwo.cn/mdd/'
+    countrys = getCountrys(url)
+    try:
+        conn = MySQLdb.connect(host="localhost", user="root", passwd="544989", db="python_pro", charset="utf8")
+        cursor = conn.cursor()
+        values = []
+        for country in countrys:
+            values.append(country.getCountry())
+        cursor.executemany('insert into tb_mfw values(%s, %s, %s, %s)', values)
+        cursor.close()
+        conn.commit()
+        conn.close()
+    except MySQLdb.Error, e:
+        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+    end = time.time()
+    print u'导入mysql执行时间：%s' % (end - start)
 
 # -------------------------主函数------------------------- #
 if __name__ == '__main__':
-    test1()
+    test3()
